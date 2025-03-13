a3c3a6be652539e5375f8ec8181be6d8fd4d1613
csmanhaes86 <charlesmanhaes1@gmail.con>
2025-03-12 23:35:53 -0300

    Aula 54 - Criando Menu lateral

--- src/components/SideNavAdmin/index.jsx
+++ src/components/SideNavAdmin/index.jsx
@@ -1,8 +1,30 @@
+import { SignOut } from '@phosphor-icons/react';
+
+import { navLinks } from './navLinks';
+import Logo from '../../assets/logo.svg';
+import { useUser } from '../../hooks/UserContext';
+import { Container, Footer, NavLink, NavLinkContainer } from './styles';
+
 export function SideNavadmin() {
-    return (
-      <div>
-        <h3>menu</h3>
-      </div>
-    );
-  }
-  
+  const { logout } = useUser;
+
+  return (
+    <Container>
+      <img src={Logo} alt="logo devburger" />
+      <NavLinkContainer>
+        {navLinks.map((link) => (
+          <NavLink key={link.id} to={link.path}>
+            {link.icon}
+            <span> {link.label}</span>
+          </NavLink>
+        ))}
+      </NavLinkContainer>
+      <Footer>
+        <NavLink to="/login" onClick={logout}>
+          <SignOut />
+          <span>Sair</span>
+        </NavLink>
+      </Footer>
+    </Container>
+  );
+}
